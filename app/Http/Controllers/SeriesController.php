5a123187c7c8fd0faadf003eadac84fe5319d2f4
Jairison Silva de Souza <116470586+jairisonsouza@users.noreply.github.com>
2023-06-29 11:42:15 -0300

    Atualizando Controllers

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function listarSeries () {
+    public function index(Request $request) {
         $series = [
             'Arrow',
             'Chigago Fire',
@@ -22,6 +22,6 @@ class SeriesController extends Controller
 
         $html .= "</ul>";
 
-        echo $html;
+        return $html;
     }
 }
