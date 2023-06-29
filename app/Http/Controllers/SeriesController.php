d17b29ac6d0f733d101a6faee63517ac83d24438
Jairison Silva de Souza <116470586+jairisonsouza@users.noreply.github.com>
2023-06-29 09:08:14 -0300

    Corrigindo SeriesController

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    //
+    public function () listarSeries {
+        $series = [
+            'Arrow',
+            'Chigago Fire',
+            'Game of Thrones',
+            'The Last of Us',
+            'The Flash'
+        ];
+
+        $html = '<ul>';
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+
+        $html .= "</ul>";
+
+        echo $html;
+    }
 }
