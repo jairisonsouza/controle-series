bbe31752bb82f989280e60c96b9442071cee77fb
Jairison Silva de Souza <116470586+jairisonsouza@users.noreply.github.com>
2023-06-29 08:54:41 -0300

    Criando Route 'Series'

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SeriesController
+{
+    public function listarSeries () {
+        $series = [
+            'Arrow',
+            'The Hookie',
+            'Chigago Fire',
+            'Game of Thrones',
+            'The Last of Us'
+        ];
+
+        $html = '<ul>';
+        foreach ($series as $serie) {
+            $html .= "<li>$series</li>";
+        }
+        $html .= </ul>;
+
+        echo $html;
+    }
+}
+
+?>
