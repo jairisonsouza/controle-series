d58f96eea39d53f9e83967c432da4f10d9845ec4
Jairison Silva de Souza <116470586+jairisonsouza@users.noreply.github.com>
2023-06-29 20:54:37 -0300

    Adicionando link para adicionar Series e atualizando rotas

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,6 @@
+<x-layout title="Nova Séries">
+    <form action="" method="post">
+        <label for="nome">Nome: </label>
+        <input type="text" id="nome" nome="nome">
+    </form>
+</x-layout>
