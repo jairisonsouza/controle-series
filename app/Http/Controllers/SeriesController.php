d0f4d67bbb034e0690768f083e7b60ff310b167c
Jairison Silva de Souza <116470586+jairisonsouza@users.noreply.github.com>
2023-06-29 20:37:34 -0300

    Criando Layout BLADE e utilizando nas views

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -15,6 +15,6 @@ class SeriesController extends Controller
             'The Flash'
         ];
 
-        return view ('listar-series')->with('series', $series); 
+        return view ('series.index')->with('series', $series); 
     }
 }
