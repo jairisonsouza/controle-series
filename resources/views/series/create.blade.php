7f6d64d09b2ae13bb133b99397013a6eb4a50958
Jairison Silva de Souza <116470586+jairisonsouza@users.noreply.github.com>
2023-06-30 18:03:44 -0300

    Módulo 1

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,8 +1,9 @@
-<x-layout title="Nova Séries">
-    <form action="" method="post">
+<x-layout title="Nova Série">
+    <form action="/series/salvar" method="post">
+        @csrf
         <div class="mb-3">
-            <label for="nome" class="form-label">Nome: </label>
-            <input type="text" id="nome" nome="nome" class="form-control">
+            <label for="nome" class="form-label">Nome:</label>
+            <input type="text" id="nome" name="nome" class="form-control">
         </div>
 
         <button type="submit" class="btn btn-primary">Adicionar</button>
