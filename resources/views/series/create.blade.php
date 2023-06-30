40db28b0b6e343530bb3c8a80d3beab784748b86
Jairison Silva de Souza <116470586+jairisonsouza@users.noreply.github.com>
2023-06-30 11:13:28 -0300

    Estilzando páginas com bootstrap

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,6 +1,10 @@
 <x-layout title="Nova Séries">
     <form action="" method="post">
-        <label for="nome">Nome: </label>
-        <input type="text" id="nome" nome="nome">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome: </label>
+            <input type="text" id="nome" nome="nome" class="form-control">
+        </div>
+
+        <button type="submit" class="btn tbn-primary">Adicionar</button>
     </form>
 </x-layout>
